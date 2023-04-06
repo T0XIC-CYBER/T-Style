0a3f26a997c77784b3c68bafe3db3e75309c90d7
TOXIC CYBER SECURITY <104613340+T0XIC-CYBER@users.noreply.github.com>
2023-04-06 07:48:14 +0600

    Create request.sh

---
+++ T-Style/request.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# @By_Arif_Hasnain
+clear
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+figlet -f big Done !!! | lolcat
+echo
+echo
+echo
+echo
